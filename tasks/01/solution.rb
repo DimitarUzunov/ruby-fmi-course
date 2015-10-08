079ff66e22e06f213ed337bad4ed44a399cc2aed
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-09 00:17:32 +0300

    added first solution to task 01

---
+++ tasks/01/solution.rb
@@ -0,0 +1,17 @@
+def convert_to_bgn(price, currency)
+  if currency == :bgn
+    price
+  elsif currency == :usd
+    (price * 1.7408).round(2)
+  elsif currency == :eur
+    (price * 1.9557).round(2)
+  elsif currency == :gbp
+    (price * 2.6415).round(2)
+  end
+end
+
+def compare_prices(first_price, first_currency, second_price, second_currency)
+  first_price_bgn = convert_to_bgn(first_price, first_currency)
+  second_price_bgn = convert_to_bgn(second_price, second_currency)
+  first_price_bgn - second_price_bgn
+end
