fb4645b93c5f28f5bb453abcef865ed4dc1951fb
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-15 17:44:18 +0300

    added more tests

--- tasks/02/spec.rb
+++ tasks/02/spec.rb
@@ -1,7 +1,32 @@
+describe '#snake_ahead_position' do
+  it 'returns snake\'s next front position in particular direction' do
+    expect(snake_ahead_position([[4, 5], [4, 6], [5, 6], [5, 7]], [0, 1])).
+      to eq [5, 8]
+
+    expect(snake_ahead_position([[4, 5], [4, 6], [5, 6], [5, 7]], [0, -1])).
+      to eq [5, 6]
+
+    expect(snake_ahead_position([[4, 5], [4, 6], [5, 6], [5, 7]], [1, 0])).
+      to eq [6, 7]
+
+    expect(snake_ahead_position([[4, 5], [4, 6], [5, 6], [5, 7]], [-1, 0])).
+      to eq [4, 7]
+  end
+end
+
 describe '#move' do
   it 'moves snake right' do
     expect(move([[4, 5], [4, 6], [5, 6], [5, 7]], [0, 1])).
       to eq [[4, 6], [5, 6], [5, 7], [5, 8]]
+
+    expect(move([[4, 5], [4, 6], [5, 6], [5, 7]], [0, -1])).
+      to eq [[4, 6], [5, 6], [5, 7], [5, 6]]
+
+    expect(move([[4, 5], [4, 6], [5, 6], [5, 7]], [1, 0])).
+      to eq [[4, 6], [5, 6], [5, 7], [6, 7]]
+
+    expect(move([[4, 5], [4, 6], [5, 6], [5, 7]], [-1, 0])).
+      to eq [[4, 6], [5, 6], [5, 7], [4, 7]]
   end
 end
 
@@ -9,6 +34,15 @@ describe '#grow' do
   it 'grows snake right' do
     expect(grow([[4, 6], [5, 6], [5, 7]], [0, 1])).
       to eq [[4, 6], [5, 6], [5, 7], [5, 8]]
+
+    expect(grow([[4, 6], [5, 6], [5, 7]], [0, -1])).
+      to eq [[4, 6], [5, 6], [5, 7], [5, 6]]
+
+    expect(grow([[4, 6], [5, 6], [5, 7]], [1, 0])).
+      to eq [[4, 6], [5, 6], [5, 7], [6, 7]]
+
+    expect(grow([[4, 6], [5, 6], [5, 7]], [-1, 0])).
+      to eq [[4, 6], [5, 6], [5, 7], [4, 7]]
   end
 end
 
@@ -23,12 +57,52 @@ describe '#obstacle_ahead?' do
   it 'returns true if wall in front of snake' do
     expect(obstacle_ahead?([[3, 8], [3, 9]], [0, 1], {width: 10, height: 10})).
       to eq true
+
+    expect(obstacle_ahead?([[3, 1], [3, 0]], [0, -1], {width: 10, height: 10})).
+      to eq true
+
+    expect(obstacle_ahead?([[8, 8], [9, 8]], [1, 0], {width: 10, height: 10})).
+      to eq true
+
+    expect(obstacle_ahead?([[1, 8], [0, 8]], [-1, 0], {width: 10, height: 10})).
+      to eq true
+  end
+
+  it 'returns true if part of snake in front of snake' do
+    expect(obstacle_ahead?([[3, 1], [3, 0]], [0, 1], {width: 10, height: 10})).
+      to eq true
+
+    expect(obstacle_ahead?([[3, 8], [3, 9]], [0, -1], {width: 10, height: 10})).
+      to eq true
+
+    expect(obstacle_ahead?([[1, 8], [0, 8]], [1, 0], {width: 10, height: 10})).
+      to eq true
+
+    expect(obstacle_ahead?([[8, 8], [9, 8]], [-1, 0], {width: 10, height: 10})).
+      to eq true
+
+    expect(obstacle_ahead?([[1, 1], [1, 2], [2, 2], [2, 3], [1, 3]], [0, -1],
+                           {width: 10, height: 10})).
+      to eq true
   end
 end
 
 describe '#danger?' do
   it 'returns true if position in front of snake is a wall' do
     expect(danger?([[3, 8], [3, 9]], [0, 1], {width: 10, height: 10})).
+      to eq true
+
+    expect(danger?([[3, 7], [3, 8]], [0, 1], {width: 10, height: 10})).
+      to eq true
+  end
+
+  it 'returns true if position in front of snake is a part of snake' do
+    expect(danger?([[1, 1], [1, 2], [2, 2], [2, 3], [1, 3]], [0, -1],
+                   {width: 10, height: 10})).
+      to eq true
+
+    expect(danger?([[1, 1], [1, 2], [2, 2], [2, 3], [2, 4], [1, 4]], [0, -1],
+                   {width: 10, height: 10})).
       to eq true
   end
 end
