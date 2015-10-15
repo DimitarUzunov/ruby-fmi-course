87681674c9648769efbd2a761fa3311c6dda1a2b
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-15 21:31:27 +0300

    updated skeptic rules

--- tasks/02/run.sh
+++ tasks/02/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running skeptic..."
-skeptic --lines-per-method 8 --line-length 80 --max-nesting-depth 1 --methods-per-class 10 --no-semicolons --no-trailing-whitespace --check-syntax --no-global-variables --english-words-for-names='xs ys' --naming-conventions --spaces-around-operators solution.rb
+skeptic --lines-per-method 8 --line-length 80 --max-nesting-depth 1 --methods-per-class 10 --no-semicolons --no-trailing-whitespace --check-syntax --no-global-variables --english-words-for-names='xs ys' --naming-conventions solution.rb
 printf "\n"
 
 echo "Running spec..."
