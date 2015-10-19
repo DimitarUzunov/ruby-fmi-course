e720156af50c69dfb785975827597cd68aa4ff0f
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-19 16:08:17 +0300

    removed unnecessary code

--- challenges/01/solution.rb
+++ challenges/01/solution.rb
@@ -1,9 +1,7 @@
 def complement(f)
-  parameters = f.parameters.map { |_, name| name }
   ->(*parameters) { not f.call(*parameters) }
 end
 
 def compose(f, g)
-  parameters = g.parameters.map { |_, name| name }
   ->(*parameters) { f.call(g.call(*parameters)) }
 end
