35a3b128d3ba6e4e2ee941a0290864dd51470d7a
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-25 20:25:33 +0200

    added Rakefiles and removed run.sh scripts

--- tasks/01/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Running skeptic..."
-skeptic --lines-per-method 10 --line-length 80 --max-nesting-depth 2 --no-semicolons --no-trailing-whitespace --check-syntax --no-global-variables --english-words-for-names='bgn usd eur gbp leva dollars euro pounds' --naming-conventions solution.rb
-printf "\n"
-
-echo "Running spec..."
-rspec spec.rb --require ./solution.rb --colour --format documentation
--- tasks/03/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Running skeptic..."
-skeptic --lines-per-method 15 --line-length 100 --max-nesting-depth 2 --methods-per-class 10 --no-semicolons --no-trailing-whitespace --check-syntax --no-global-variables --english-words-for-names='fibonacci' --naming-conventions solution.rb
-printf "\n"
-
-echo "Running spec..."
-rspec spec.rb --require ./solution.rb --colour --format documentation
