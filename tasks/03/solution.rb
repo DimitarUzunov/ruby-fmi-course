a4328e51fc357be70615b7a6a73de6b47a5152e1
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-27 10:47:18 +0200

    improved solution

--- tasks/03/solution.rb
+++ tasks/03/solution.rb
@@ -1,70 +1,51 @@
 class Integer
   def prime?
     return false if self == 1
-
-    root = Math.sqrt(self)
-    divisor = 2
-
-    while divisor <= root
-      return false if self % divisor == 0
-      divisor += 1
-    end
-
-    true
+    2.upto(self ** 0.5).all? { |divisor| self % divisor != 0 }
   end
 end
 
 class RationalSequence
   include Enumerable
 
-  def initialize(limit)
-    @limit = limit
+  def initialize(count)
+    @count = count
   end
 
-  def each
-    current = Rational(1)
-    direction = DIRECTIONS[:down]
-    counter = 0
-
-    while counter < @limit
-      yield current
-
-      current = calculate_next(current, direction)
-      direction = change_direction(current, direction)
-      counter += 1
-    end
+  def each(&block)
+    enum_for(:all_ordered_pairs).
+      lazy.
+      select { |numerator, denominator| numerator.gcd(denominator) == 1 }.
+      map { |numerator, denominator| Rational(numerator, denominator) }.
+      take(@count).
+      each(&block)
   end
 
   private
 
-  DIRECTIONS = {down: [1, 0], right: [0, 1], down_left: [1, -1], up_right: [-1, 1]}
+  def all_ordered_pairs
+    numerator, denominator = [1, 1]
 
-  def reducible?(numerator, denominator)
-    numerator.gcd(denominator) != 1
-  end
+    loop do
+      yield [numerator, denominator]
 
-  def calculate_next_irreducible(numerator, denominator, direction)
-    if reducible?(numerator, denominator)
-      calculate_next_irreducible(numerator + direction[0], denominator + direction[1], direction)
-    else
-      Rational(numerator, denominator)
-    end
-  end
+      numerator += 1
 
-  def calculate_next(current, direction)
-    numerator = current.numerator + direction[0]
-    denominator = current.denominator + direction[1]
+      while numerator > 1
+        yield [numerator, denominator]
+        numerator -= 1
+        denominator += 1
+      end
 
-    calculate_next_irreducible(numerator, denominator, direction)
-  end
+      yield [numerator, denominator]
 
-  def change_direction(current, direction)
-    case
-      when direction == DIRECTIONS[:down] then DIRECTIONS[:up_right]
-      when direction == DIRECTIONS[:right] then DIRECTIONS[:down_left]
-      when (direction == DIRECTIONS[:down_left] and current.denominator == 1) then DIRECTIONS[:down]
-      when (direction == DIRECTIONS[:up_right] and current.numerator == 1) then DIRECTIONS[:right]
-      else direction
+      denominator += 1
+
+      while denominator > 1
+        yield [numerator, denominator]
+        numerator += 1
+        denominator -= 1
+      end
     end
   end
 end
@@ -72,21 +53,24 @@ end
 class PrimeSequence
   include Enumerable
 
-  def initialize(limit)
-    @limit = limit
+  def initialize(count)
+    @count = count
   end
 
-  def each
-    current = 2
-    counter = 0
+  def each(&block)
+    enum_for(:all_primes).
+      lazy.
+      take(@count).
+      each(&block)
+  end
 
-    while counter < @limit
-      if current.prime?
-        yield current
-        counter += 1
-      end
+  private
 
-      current += 1
+  def all_primes
+    current_number = 2
+    loop do
+      yield current_number if current_number.prime?
+      current_number += 1
     end
   end
 end
@@ -94,21 +78,28 @@ end
 class FibonacciSequence
   include Enumerable
 
-  def initialize(limit, first: 1, second: 1)
-    @limit = limit
+  def initialize(count, first: 1, second: 1)
+    @count = count
     @first = first
     @second = second
   end
 
-  def each
-    current_number, next_number = @first, @second
-    counter = 0
+  def each(&block)
+    enum_for(:all_numbers).
+      lazy.
+      take(@count).
+      each(&block)
+  end
 
-    while counter < @limit
-      yield current_number
+  def all_numbers
+    previous, current = @first, @second
 
-      current_number, next_number = next_number, current_number + next_number
-      counter += 1
+    yield previous
+    yield current
+
+    loop do
+      yield previous + current
+      previous, current = current, previous + current
     end
   end
 end
@@ -117,33 +108,26 @@ module DrunkenMathematician
   module_function
 
   def meaningless(n)
-    groups = RationalSequence.new(n).partition do |rational|
-      rational.numerator.prime? or rational.denominator.prime?
-    end
+    sequence = RationalSequence.new(n)
+    primeish, non_primeish = sequence.partition { |r| r.numerator.prime? or r.denominator.prime? }
 
-    groups[0].reduce(1, :*) / groups[1].reduce(1, :*)
+    primeish.reduce(1, :*) / non_primeish.reduce(1, :*)
   end
 
   def aimless(n)
-    pairs = PrimeSequence.new(n).each_slice(2).to_a
-    pairs.last << 1 if n.odd?
-
-    rationals = pairs.map { |pair| Rational(pair[0], pair[1]) }
-    rationals.reduce(0, :+)
+    sequence = PrimeSequence.new(n)
+    sequence.each_slice(2).map { |first, second| Rational(first, second || 1) }.reduce(0, :+)
   end
 
   def worthless(n)
     return [] if n == 0
 
-    fibonacci_number = FibonacciSequence.new(n).to_a.last
+    limit = FibonacciSequence.new(n).to_a.last
+    sum = 0
 
-    cut_size = 1
-    rationals_cut = RationalSequence.new(cut_size)
-    while rationals_cut.reduce(:+) <= fibonacci_number
-      cut_size += 1
-      rationals_cut = RationalSequence.new(cut_size)
+    RationalSequence.new(limit ** 2).take_while do |rational|
+      sum += rational
+      sum <= limit
     end
-
-    RationalSequence.new(cut_size - 1).to_a
   end
 end
